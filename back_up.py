dbc1fc9a4b24f57bbdba3bcfbcba50b6caf134f7
mokimokheonpark <105946843+mokimokheonpark@users.noreply.github.com>
2022-07-14 17:59:26 -0400

    Add files via upload

---
+++ back_up.py
@@ -0,0 +1,142 @@
+import pandas
+import numpy
+import sklearn.feature_extraction.text
+import sklearn.metrics.pairwise
+import ast
+
+credits_data = pandas.read_csv("tmdb_5000_credits.csv")
+movies_data = pandas.read_csv("tmdb_5000_movies.csv")
+
+credits_data.columns = ["id", "title", "cast", "crew"]
+
+data = movies_data.merge(credits_data[["id", "cast", "crew"]], on = "id")
+
+####################################################################################################
+
+# Movie Recommendation based on overview(main plot points, storyline, etc.)
+
+tfidf = sklearn.feature_extraction.text.TfidfVectorizer(stop_words = "english")
+data["overview"] = data["overview"].fillna("")
+tfidf_matrix = tfidf.fit_transform(data["overview"])
+
+cosine_similarity1 = sklearn.metrics.pairwise.linear_kernel(tfidf_matrix, tfidf_matrix)
+
+####################################################################################################
+
+
+
+####################################################################################################
+
+# Movie Recommendation based on cast, director, genres and keywords
+
+features1 = ["cast", "crew", "genres", "keywords"]
+for feature in features1:
+    data[feature] = data[feature].apply(ast.literal_eval)
+
+def get_director(x):
+    for i in x:
+        if i["job"] == "Director":
+            return i["name"]
+    return numpy.nan
+
+data["director"] = data["crew"].apply(get_director)
+
+def get_list(x):
+    if isinstance(x, list):
+        names = [i["name"] for i in x]
+        if len(names) > 3:
+            names = names[:3]
+        return names
+    return []
+
+features2 = ["cast", "genres", "keywords"]
+for feature in features2:
+    data[feature] = data[feature].apply(get_list)
+
+def adjust_data(x):
+    if isinstance(x, list):
+        return [str.lower(i.replace(" ", "")) for i in x]
+    else:
+        if isinstance(x, str):
+            return str.lower(x.replace(" ", ""))
+        else:
+            return ""
+
+features3 = ["cast", "director", "genres", "keywords"]
+for feature in features3:
+    data[feature] = data[feature].apply(adjust_data)
+
+def concatenate_data(x):
+    cast = " ".join(x["cast"])
+    director = x["director"]
+    genres = " ".join(x["genres"])
+    keywords = " ".join(x["keywords"])
+    return cast + " " + director + " " + genres + " " + keywords
+
+data["concatenated_data"] = data.apply(concatenate_data, axis = 1)
+
+count = sklearn.feature_extraction.text.CountVectorizer(stop_words = "english")
+count_matrix = count.fit_transform(data["concatenated_data"])
+
+cosine_similarity2 = sklearn.metrics.pairwise.cosine_similarity(count_matrix, count_matrix)
+
+####################################################################################################
+
+data = data.reset_index()
+movies = data[["id", "title"]].copy()
+
+
+
+# Movie Recommendation based on cast, director, genres and keywords
+
+import tmdbv3api
+import streamlit
+
+movie = tmdbv3api.Movie()
+tmdb = tmdbv3api.TMDb()
+
+tmdb.api_key = "a9a70d6e2834d25889ca3465084e5765"
+
+def get_recommendations(title):
+    index = movies[movies["title"] == title].index[0]
+
+    cosine_similarity_scores = list(enumerate(cosine_similarity2[index]))
+    cosine_similarity_scores = sorted(cosine_similarity_scores, key = lambda x: x[1], reverse = True)
+    cosine_similarity_scores = cosine_similarity_scores[1:26]
+
+    movie_indices = [i[0] for i in cosine_similarity_scores]
+    images = []
+    titles = []
+
+    for i in movie_indices:
+        id = movies["id"].iloc[i]
+        details = movie.details(id)
+        image_path = details["poster_path"]
+
+        if image_path:
+            image_path = "https://image.tmdb.org/t/p/w500" + image_path
+        else:
+            image_path = "no_image.jpg"
+
+        images.append(image_path)
+        titles.append(details["title"])
+    return images, titles
+
+streamlit.set_page_config(layout = "wide")
+streamlit.header("Movie Recommendation based on cast, director, genres and keywords")
+
+movie_list = movies["title"].values
+title = streamlit.selectbox("Choose a movie you like", movie_list)
+
+if streamlit.button("Get Recommendations"):
+    with streamlit.spinner("Loading..."):
+        images, titles = get_recommendations(title)
+        index = 0
+        for _ in range(5):
+            cols = streamlit.columns(5)
+            for col in cols:
+                col.image(images[index])
+                col.write(titles[index])
+                index += 1
+
+# To run the program and open the wab page, enter "streamlit run movie_recommendation_app2.py" as input in terminal
